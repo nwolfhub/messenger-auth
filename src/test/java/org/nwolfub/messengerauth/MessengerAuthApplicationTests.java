514e5fbb9738a9c55d58673d58eab8b03be302bc
s0m31 <admin@nwolfhub.org>
2022-10-18 20:55:08 +0300

    Init project

---
+++ src/test/java/org/nwolfub/messengerauth/MessengerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nwolfub.messengerauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
